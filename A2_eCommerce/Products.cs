8bf46ff62468efaab0c3a157ec0c3eb8e8ab5996
VenicioAugusto <79486034+VenicioAugusto@users.noreply.github.com>
2025-01-25 22:41:55 -0500

    Add Products class attributes and methods

--- A2_eCommerce/Products.cs
+++ A2_eCommerce/Products.cs
@@ -8,5 +8,26 @@ namespace A2_eCommerce
 {
     public class Products
     {
+        int ProdID { get; set; }
+        string ProdName { get; set; }
+        int ItemPrice { get; set; }
+        int StockAmount { get; set; }
+
+        public Products(int prodID, string prodName, int itemPrice, int stockAmount) { 
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        public void IncreaseStock()
+        {
+            if (StockAmount < 100000) { StockAmount++; }            
+        }
+
+        public void DecreaseStock()
+        {
+            if (StockAmount > 1) { StockAmount--; }
+        }
     }
 }

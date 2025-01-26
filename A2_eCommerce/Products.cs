405aa2589345dfdc5f725fdd4e6f7a920378b59b
VenicioAugusto <79486034+VenicioAugusto@users.noreply.github.com>
2025-01-26 11:34:16 -0500

    Add validation to Product class

--- A2_eCommerce/Products.cs
+++ A2_eCommerce/Products.cs
@@ -8,26 +8,43 @@ namespace A2_eCommerce
 {
     public class Products
     {
-        int ProdID { get; set; }
-        string ProdName { get; set; }
-        int ItemPrice { get; set; }
-        int StockAmount { get; set; }
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public int ItemPrice { get; set; }
+        public int StockAmount { get; set; }
 
-        public Products(int prodID, string prodName, int itemPrice, int stockAmount) { 
+        public Products(int prodID, string prodName, int itemPrice, int stockAmount) {
+            if (prodID < 10 || prodID > 100000)
+                throw new ArgumentOutOfRangeException(nameof(prodID), "ProdID must be between 10 and 100000.");
+            if (itemPrice < 10 || itemPrice > 10000)
+                throw new ArgumentOutOfRangeException(nameof(itemPrice), "Item price must be between $10 and $10,000.");
+            if (stockAmount < 1 || stockAmount > 100000)
+                throw new ArgumentOutOfRangeException(nameof(stockAmount), "Stock amount must be between 1 and 100,000.");
+
             ProdID = prodID;
-            ProdName = prodName;
+            ProdName = prodName ?? throw new ArgumentNullException(nameof(prodName));
             ItemPrice = itemPrice;
             StockAmount = stockAmount;
         }
 
-        public void IncreaseStock()
+        public void IncreaseStock(int amount = 1)
         {
-            if (StockAmount < 100000) { StockAmount++; }            
+            if (amount < 1)
+                throw new ArgumentOutOfRangeException(nameof(amount), "Increase amount must be at least 1.");
+            if (StockAmount + amount > 100000)
+                throw new InvalidOperationException("Stock amount cannot exceed 100,000.");
+
+            StockAmount += amount;
         }
 
-        public void DecreaseStock()
+        public void DecreaseStock(int amount = 1)
         {
-            if (StockAmount > 1) { StockAmount--; }
+            if (amount < 1)
+                throw new ArgumentOutOfRangeException(nameof(amount), "Decrease amount must be at least 1.");
+            if (StockAmount - amount < 1)
+                throw new InvalidOperationException("Stock amount cannot be less than 1.");
+
+            StockAmount -= amount;
         }
     }
 }
--- eCommerce_Tests/ProductsTests.cs
+++ eCommerce_Tests/ProductsTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using A2_eCommerce;
 
 namespace eCommerce_Tests
 {
+    [TestFixture]
     public class ProductsTests
     {
+        
+
     }
 }

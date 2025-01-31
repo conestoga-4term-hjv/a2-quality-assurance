a1fa5bcc1ef3b3c0582ad9f964ac878cd430ac01
VenicioAugusto <79486034+VenicioAugusto@users.noreply.github.com>
2025-01-30 22:41:08 -0500

    Update tests names based on feedback from professor

--- eCommerce_Tests/ProductsTests.cs
+++ eCommerce_Tests/ProductsTests.cs
@@ -38,58 +38,61 @@ namespace eCommerce_Tests
         // For this test, I'm testing creation with an invalid stock, a negative one, more precisely
         // I designed this one because it could be a very problematic one if it occurs, and it is also very easy to occur
         [Test]
-        public void Constructor_NegativeStockAmount_ShouldThrowArgumentOutOfRangeException()
+        public void StockAmount_Negative10_ShouldThrowArgumentOutOfRangeException()
         {
             // Arrange
             int validProdID = 101;
             string validProdName = "Test Product";
             int validItemPrice = 500;
             int invalidStockAmount = -10;
+            string message = "Stock amount must be between 1 and 100,000.";
 
             // Act
             ArgumentOutOfRangeException ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
                 new Products(validProdID, validProdName, validItemPrice, invalidStockAmount));
 
             // Assert
-            Assert.That(ex.Message, Does.Contain("Stock amount must be between 1 and 100,000."));
+            Assert.That(ex.Message, Does.Contain(message));
         }
 
         // For this test, I'm testing creation with a value higher than the accepted value
         // I designed this one because users might assign value wrongly, and we need to make sure the system is catching that
         [Test]
-        public void Constructor_10001PriceValue_ShouldThrowArgumentOutOfRangeException()
+        public void ItemPrice_10001_ShouldThrowArgumentOutOfRangeException()
         {
             // Arrange
             int validProdID = 102;
             string validProdName = "Test Product";
             int invalidItemPrice = 10001;
             int validStockAmount = 1;
+            string message = "Item price must be between $10 and $10,000.";
 
             // Act
             ArgumentOutOfRangeException ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
                 new Products(validProdID, validProdName, invalidItemPrice, validStockAmount));
 
             // Assert
-            Assert.That(ex.Message, Does.Contain("Item price must be between $10 and $10,000."));
+            Assert.That(ex.Message, Does.Contain(message));
         }
 
         // For this test, I'm testing creation with an invalid ProductID
         // This should be a fairly common error and the system should be able to catch it
         [Test]
-        public void Constructor_InvalidProductID_ShouldThrowArgumentOutOfRangeException()
+        public void ProdID_5_ShouldThrowArgumentOutOfRangeException()
         {
             // Arrange
             int invalidProdID = 5;
             string validProdName = "Test Product";
             int validItemPrice = 1000;
             int validStockAmount = 10;
+            string message = "Product ID must be between 10 and 100000.";
 
             // Act
             ArgumentOutOfRangeException ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
                 new Products(invalidProdID, validProdName, validItemPrice, validStockAmount));
 
             // Assert
-            Assert.That(ex.Message, Does.Contain("Product ID must be between 10 and 100000."));
+            Assert.That(ex.Message, Does.Contain(message));
         }
 
         #endregion
@@ -99,13 +102,14 @@ namespace eCommerce_Tests
         // For this test, I'm testing increasing the stock to a value that exceeds the maximum capacity
         // Exceeding the maximum capacity should be a common error, as such, we need to cover it 
         [Test]
-        public void IncreaseStock_ExceedsMax_ShouldThrowInvalidOperationException()
+        public void IncreaseStock_100001_ShouldThrowInvalidOperationException()
         {
             // Arrange
             int validProdID = 100;
             string validProdName = "Test Product";
             int validItemPrice = 500;
             int validStockAmount = 50;
+            string message = "Stock amount cannot exceed 100,000.";
 
             Products validProduct = new Products(validProdID, validProdName, validItemPrice, validStockAmount);
 
@@ -115,19 +119,20 @@ namespace eCommerce_Tests
             InvalidOperationException ex = Assert.Throws<InvalidOperationException>(() => validProduct.IncreaseStock(increaseAmount));
 
             // Assert
-            Assert.That(ex.Message, Does.Contain("Stock amount cannot exceed 100,000."));
+            Assert.That(ex.Message, Does.Contain(message));
         }
 
         // For this test, I'm testing decreasing the stock below zero
         // Selling more items than you have should be a common error to occur, and the system should prevent that
         [Test]
-        public void DescreaseStock_LessThanCurrentStock_ShouldThrowInvalidOperationException()
+        public void DescreaseStock_Decreate11From10_ShouldThrowInvalidOperationException()
         {
             // Arrange
             int validProdID = 100;
             string validProdName = "Test Product";
             int validItemPrice = 500;
             int validStockAmount = 10;
+            string message = "Stock amount cannot be less than 1.";
 
             Products validProduct = new Products(validProdID, validProdName, validItemPrice, validStockAmount);
 
@@ -137,13 +142,13 @@ namespace eCommerce_Tests
             InvalidOperationException ex = Assert.Throws<InvalidOperationException>(() => validProduct.DecreaseStock(descreaseAmount));
 
             // Assert
-            Assert.That(ex.Message, Does.Contain("Stock amount cannot be less than 1."));
+            Assert.That(ex.Message, Does.Contain(message));
         }
 
         // For this test, I'm testing decreasing the stock by a valid amount
         // This test is design to make sure the stock is being decreased correctly
         [Test]
-        public void DescreaseStock_ValidAmount10_ShouldDecreaseStock()
+        public void DescreaseStock_10_ShouldDecreaseStock()
         {
             // Arrange
             int validProdID = 100;
